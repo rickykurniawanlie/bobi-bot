7ced664c2c26350450f72c0222ad7a6ef9c3a654
Ricky Kurniawan <rickykurniawanlie@gmail.com>
2017-11-02 11:19:54 +0700

    Connect app with Line

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var bot = require('./routes/bot');
 
 var app = express();
 
@@ -15,9 +16,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+app.use('/linebot', bot);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
